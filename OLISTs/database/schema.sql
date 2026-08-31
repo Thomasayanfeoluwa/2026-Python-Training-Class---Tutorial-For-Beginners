2168d47b96efcddaae73a3cde3f72c8695c6cb64
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-08-31 21:07:04 +0100

    checkpoint(data): update project files

--- OLISTs/database/schema.sql
+++ OLISTs/database/schema.sql
@@ -43,11 +43,11 @@ CREATE TABLE sellers (
 CREATE TABLE products (
     product_id TEXT PRIMARY KEY,
     product_category_name TEXT,
-    product_name_length INTEGER,
-    product_description_length INTEGER,
+    product_name_lenght INTEGER,
+    product_description_lenght INTEGER,
     product_photos_qty INTEGER,
     product_weight_g INTEGER,
-    product_length_cm INTEGER,
+    product_lenght_cm INTEGER,
     product_height_cm INTEGER,
     product_width_cm INTEGER
 );
